4cc6664d80c496df9f599db5e30588728d99da05
Danieltq04 <daniel.tq04@gmail.com>
2022-06-22 14:37:26 -0300

    Popup inicial agregado

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+
+let button = document.querySelector(".visitas");
+
+
+button.addEventListener("click",()=>{
+    console.log("Presionado")
+    window.open("./html/popup.html","Popup","statusbar=no, width=800,height=400")
+})
+
