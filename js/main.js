1e77cb12f9eb5afd1014fcf6e25b764cf4af49de
Danieltq04 <daniel.tq04@gmail.com>
2022-06-22 14:50:09 -0300

    header transparent transition

--- js/main.js
+++ js/main.js
@@ -8,3 +8,17 @@ button.addEventListener("click",()=>{
     window.open("./html/popup.html","Popup","statusbar=no, width=800,height=400")
 })
 
+
+
+
+
+const nav = document.querySelector('body header')
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if(window.scrollY > nav.offsetHeight + 40) {
+        nav.classList.add('active')
+    } else {
+        nav.classList.remove('active')
+    }
+}
