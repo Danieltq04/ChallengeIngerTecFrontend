1b204f5618a6551ecfddbb532fb8bf6510d696cc
Danieltq04 <daniel.tq04@gmail.com>
2022-06-22 16:52:15 -0300

    Función para cargar por medio de ajax

--- js/main.js
+++ js/main.js
@@ -38,3 +38,17 @@ function fixNav() {
 }
 
 
+
+/* Cargar desde ajax */
+
+const url = '../home.json'
+const http = new XMLHttpRequest()
+
+http.open("GET", url)
+
+http.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+        console.log(JSON.parse(http.responseText))
+    }
+}
+http.send()
