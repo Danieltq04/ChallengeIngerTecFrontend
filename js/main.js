9df7506ed0c9afb03907c9b1754cb39ff159484d
Danieltq04 <daniel.tq04@gmail.com>
2022-06-23 23:57:30 -0300

    Clean code

--- js/main.js
+++ js/main.js
@@ -1,41 +1,69 @@
+let popup;
 
+window.onload = () => {
+    let enlace, refviews;
+    let imgs = document.querySelectorAll("main > .img-article> img");
+    for (var i = 0; i < imgs.length; i++){
+        imgs[i].addEventListener("click",function (e) { 
+            if(this.dataset.paridad == "par"){
+                document.getElementById("modal-img").src = this.src;
+                $('#paresModal').modal('show');
 
+                document.getElementById("refvisitas"+this.dataset.refvisitas).innerHTML= parseInt(document.getElementById("refvisitas"+this.dataset.refvisitas).innerHTML)+1;
+            }
+            else{
+                enlace = this.src;
+                refviews = this.dataset.refvisitas;
 
-
-
-const nav = document.querySelector('body header')
-window.addEventListener('scroll', fixNav)
-
-function fixNav() {
-    if(window.scrollY > nav.offsetHeight + 40) {
-        nav.classList.add('active')
-    } else {
-        nav.classList.remove('active')
+                popup = window.open("./html/popup.html","Popup","statusbar=no, width=800,height=400");
+                
+                popup.addEventListener("DOMContentLoaded", function () {
+                    popup.enviar(enlace,refviews);
+                });
+            }
+        })
     }
+};
+
+/* Recibe la referencia a un artículo e incrementa el contador del articulo referenciado */
+function incrementarVisita(referenciaImg) {
+    document.getElementById("refvisitas"+referenciaImg).innerHTML= parseInt(document.getElementById("refvisitas"+referenciaImg).innerHTML)+1;
 }
 
+/* Cierra un popup */
+function cerrarVentana(){
+    popup.close()
+}
+
+/* Agrega la clase active al header al hacer scroll */
+const header = document.querySelector('body header')
+window.addEventListener('scroll', ()=>{
+    if(window.scrollY > header.offsetHeight + 40) {
+        header.classList.add('active')
+    } else {
+        header.classList.remove('active')
+    }
+})
 
 
-/* Cargar desde ajax */
+
+/* Cargar texto en un textarea mediante una petición por ajax */
 const url = '../home.json'
 const http = new XMLHttpRequest()
-
 let textarea = document.getElementById("value-textarea");
+
 document.getElementById("load-textarea").addEventListener("click",()=>{
     http.open("GET", url)
-
     http.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
-            //console.log(JSON.parse(http.responseText))
             textarea.innerHTML = JSON.parse(http.responseText).texto;
-            console.log(textarea.innerHTML)
             document.getElementById("title-ajax").innerHTML = "Texto cargado mediante AJAX"
         }
     }
     http.send()
 })
 
-
+/* Funcionamiento de los enlaces a las redes sociales */
 document.querySelector(".social-github").addEventListener("click",()=>{
     window.open("https://github.com/Danieltq04")
 })
@@ -45,88 +73,3 @@ document.querySelector(".social-youtube").addEventListener("click",()=>{
 document.querySelector(".social-linkedin").addEventListener("click",()=>{
     window.open("https://www.linkedin.com/in/danieltq04/")
 })
-
-
-
-// imgs.forEach(element => {
-//     element.addEventListener("click",()=>{
-//         console.log(this)
-//     })
-// });
-let popup;
-
-window.onload = () => {
-    let imgs = document.querySelectorAll("main > .img-article> img");
-    console.log(imgs)
-    for (var i = 0; i < imgs.length; i++){
-        console.log("a")
-        imgs[i].addEventListener("click",function (e) { 
-            //console.log(this.dataset.valor) 
-            //console.log(this.src) 
-            //$('#paresModal').modal('show');
-            //i%2
-            if(this.dataset.paridad == "par"){
-                console.log("Es par")
-                $('#paresModal').modal('show');
-                document.getElementById("modal-img").src = this.src;
-                console.log(document.getElementById("modal-img").src)
-
-                document.getElementById("refvisitas"+this.dataset.refvisitas).innerHTML= parseInt(document.getElementById("refvisitas"+this.dataset.refvisitas).innerHTML)+1;
-
-            }
-            else{
-                console.log("Es impar")
-                console.log(this.src)
-                console.log(this.src.split("https://")[1])
-                console.log(String(this.src))
-                console.log("a"+this.src)
-                console.log(typeof(this.src));
-                let enlace = this.src;
-                let refviews = this.dataset.refvisitas;
-                popup;
-                popup = window.open("./html/popup.html","Popup","statusbar=no, width=800,height=400");
-                
-                popup.addEventListener("DOMContentLoaded", function () {
-                    console.log("Ventana abierta lista!");
-                    //popup.enviar("Hola. Enviando datos desde la ventana asdspadre a la hija");
-                    popup.enviar(enlace,refviews);
-                    // popup.enviar(""+this.src);
-                    // popup.enviar(String(this.src));
-                    // popup.enviar(this.src.split("https://")[1]);
-                });
-                
-                // Definición de función
-                
-
-
-
-            }
-        })
-    }
-};
-
-function incrementarVisita(referenciaImg) {
-    console.log("Mensaje recibido de hijo: ", referenciaImg)
-    document.getElementById("refvisitas"+referenciaImg).innerHTML= parseInt(document.getElementById("refvisitas"+referenciaImg).innerHTML)+1;
-}
-function cerrarVentana(){
-    popup.close()
-}
-// let button = document.querySelector(".visitas");
-
-// let popup;
-// button.addEventListener("click",()=>{
-//     console.log("Presionado")
-//     popup = window.open("./html/popup.html","Popup","statusbar=no, width=800,height=400");
-    
-//     popup.addEventListener("DOMContentLoaded", function () {
-//         console.log("Ventana abierta lista!");
-//         popup.enviar("Hola. Enviando datos desde la ventana asdspadre a la hija");
-//     });
-    
-// })
-// // Definición de función
-// function establecerMensaje(m) {
-// 	console.log("Mensaje recibido de hijo: ", m)
-// }
-
